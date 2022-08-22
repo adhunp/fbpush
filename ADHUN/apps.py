b12973ff9de88ebeea39c4b71b27ffe57fc4029c
adhunp <adhunp8@gmail.com>
2022-08-22 12:10:36 +0530

    fbpush2

---
+++ ADHUN/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdhunConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ADHUN'
---
+++ ADHUN/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+def adhun(request):
+    return render (request,'adhun.html')
